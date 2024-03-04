df53f654130950d10e5ebf2f630f4de96f6ac90b
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-04 00:58:10 +0000

    Add Bootstrap and React Bootstrap dependencies

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import "./hero.css";
 import ButtonComponent from "../ButtonComponent";
+import logoImage from "/src/assets/me.jpeg"; // Ensure the path is correct
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1 className="hero-title w-full mx-auto lg:w-[740px] py-44">
-          Hey, I'm Victor{" "}
+        <img src={logoImage} alt="Logo" className="hero-logo" />
+        <div className="title-and-button">
+          <h1 className="hero-title">Hey, I'm Victor</h1>
           <ButtonComponent>Available for hiring</ButtonComponent>
-        </h1>
-        <p className="hero-description">
+        </div>
+        <article className="hero-description">
           <h2>
             +1 year experience. Web developer and App creator. From Leicester,
             United Kingdom 🇬🇧. Specialized in crafting unique digital
             experiences
           </h2>
-        </p>
+        </article>
         <button className="cv-button">Download CV</button>
       </div>
     </section>
