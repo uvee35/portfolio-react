5953fa351427021c6baee0e603ba44e74aa975eb
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-05 11:59:45 +0000

    Add Tailwind CSS imports and Next UIProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { UseEffect, UseState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@fontsource-variable/onest";
+import { NextUIProvider } from "@nextui-org/react"; // Import NextUIProvider
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
 import About from "./components/About/about";
@@ -10,20 +11,23 @@ import Footer from "./components/Footer/footer";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/footer" element={<Footer />} />
-          {/* Define more routes as needed */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <NextUIProvider>
+      {" "}
+      {/* */}
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* More rutes below uf necessary ✅ */}
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </NextUIProvider>
   );
 }
 
