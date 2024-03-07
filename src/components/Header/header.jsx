8705f8ba55dae49ede4fcca5c99d2e7c3f96d121
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-07 15:17:52 +0000

    Update header styling and links

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,22 +1,25 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import "./header.css"
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" style={{ backgroundColor: 'transparent' }}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" style={{ color: 'white', fontSize: '34px', fontWeight: 'bold' }}>
+          Uvee<span className="text-warning" style={{ color: '#ffc107', fontSize: '34px', fontWeight: 'bold' }}>Dev</span>
+        </Navbar.Brand>
+      
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/" style={{ color: 'white', fontSize: '20px' }}>Home</Nav.Link>
+            <Nav.Link href="/Projects" style={{ color: 'white', fontSize: '20px' }}>Projects</Nav.Link>
+            <Nav.Link href="/Contact" style={{ color: 'white', fontSize: '20px' }}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
-
 export default Header;
