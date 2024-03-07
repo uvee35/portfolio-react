cc366471fda17c484ebe70ec256af00bcd8c886e
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-07 00:00:55 +0000

    Refactor header component and update styles

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,33 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./header.css"; //
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-const Header = () => {
+function Header() {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top bg-black shadow-sm">
-      <div className="container d-flex justify-content-between">
-        <div className="logo d-flex align-items-center">
-          <span className="logo-uvee">Uvee</span>
-          <span className="logo-dev">Dev</span>
-        </div>
-        <div className="pages-link">
-          <Link to="/" className="home-link mx-2">
-            Home
-          </Link>
-          <Link to="/projects" className="project-link mx-2">
-            Projects
-          </Link>
-          <Link to="/about" className="about-me-link mx-2">
-            Who am I?
-          </Link>
-          <Link to="/contact" className="contact-link mx-2">
-            Contact
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default Header;
