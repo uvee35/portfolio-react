0e79df13cbcac7933475151ab33828ae654c3b71
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-08 23:41:33 +0000

    Refactor routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@fontsource-variable/onest";
-import { NextUIProvider } from "@nextui-org/react";
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
 import About from "./components/About/about";
@@ -9,26 +8,19 @@ import Projects from "./components/Projects/projects";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 
-function App() {
+const App = () => {
   return (
-    <NextUIProvider>
-      {" "}
-      {/* */}
-      <Router>
-        <div>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
-            {/* More rutes below uf necessary ✅ */}
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    </NextUIProvider>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />{" "}
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
