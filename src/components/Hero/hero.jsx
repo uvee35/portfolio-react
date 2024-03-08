f987b017d68e8068f5a50dc4befcab3f2d116e68
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-08 06:36:03 +0000

    Update Hero component styles for responsive design

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,49 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./hero.css";
 
 import logoImage from "/src/assets/me.jpeg";
 
 function Hero() {
+  const initialMessage = "Hey, I'm Victor";
+  const [displayText, setDisplayText] = useState("");
+  const [currentCharIndex, setCurrentCharIndex] = useState(0);
+
+  // Typing effect
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      if (currentCharIndex < initialMessage.length) {
+        setDisplayText(
+          (prevText) => prevText + initialMessage[currentCharIndex]
+        );
+        setCurrentCharIndex((prevIndex) => prevIndex + 1);
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 100); // Typing speed
+
+    return () => clearInterval(typingInterval);
+  }, [currentCharIndex]);
+
   return (
     <section
       className="hero d-flex align-items-center"
       style={{
         height: "100vh",
-        marginTop: "20px",
+        marginTop: "px",
         paddingTop: "0px",
       }}
     >
       <div className="container">
         <div className="row">
-          <div className="col-lg-12 d-flex flex-column align-items-center">
-            <img
-              src={logoImage}
-              alt="Logo"
-              style={{
-                borderRadius: "50%",
-                width: "120px",
-                height: "120px",
-                objectFit: "cover",
-                marginBottom: "20px",
-              }}
-            />
-            <h1 style={{ fontSize: "3rem", marginBottom: "10px" }}>
-              Hey, I'm Victor
-            </h1>
-            <span
-              style={{
-                backgroundColor: "#2E8B57",
-                color: "white",
-                padding: "1px 4px",
-                borderRadius: "4px",
-                marginBottom: "10px",
-              }}
-            >
-              Available for hiring
-            </span>
+          <div className="col-lg-9 d-flex align-items-center">
+            <div className="d-flex flex-column">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "-50px",
+                }}
+              ></div>
+              <img
+                src={logoImage}
+                alt="Logo"
+                style={{
+                  borderRadius: "50%",
+                  width: "200px",
+                  height: "200px",
+                  objectFit: "cover",
+                  marginBottom: "20px",
+                }}
+              />
+              <h1
+                style={{
+                  fontSize: "8rem",
+                  marginBottom: "70px",
+                  fontFamily: "Arial, sans-serif",
+                  textAlign: "center",
+                }}
+              >
+                {displayText}
+              </h1>
+            </div>
           </div>
+          <div className="col-lg-3 d-flex align-items-center justify-content-end"></div>
         </div>
-        <p style={{ fontSize: "1.5rem", textAlign: "center" }}>
+        <p
+          style={{
+            fontSize: "2rem",
+            textAlign: "center",
+            fontFamily: "cursive",
+          }}
+        >
           +1 year experience. Web developer and App creator. From Leicester,
           United Kingdom 🇬🇧.
         </p>
@@ -57,16 +89,16 @@ function Hero() {
           <a
             href="/src/assets/Cv-victor.pdf"
             download="Cv-victor.pdf"
-            className="download-btn" // Add class for hover effect
+            className="download-btn"
             style={{
-              fontSize: "1.25rem",
+              fontSize: "1.5rem",
               textDecoration: "none",
-              backgroundColor: "#ffc107", // Example color
+              backgroundColor: "#ffc107",
               color: "black",
-              padding: "3px 6px",
-              borderRadius: "5px",
+              padding: "15px 30px",
+              borderRadius: "8px",
               marginRight: "10px",
-              display: "inline-block", // Ensure the element respects padding and borderRadius
+              display: "inline-block",
             }}
           >
             Download CV
@@ -74,15 +106,15 @@ function Hero() {
           <a
             href="https://wa.me/447752117594?text=Hey%20Victor,%20I%20just%20saw%20your%20portfolio%20and%20I%20would%20like%20to%20chat."
             target="_blank"
-            className="chat-btn" // Add class for hover effect
+            className="chat-btn"
             style={{
-              fontSize: "1.25rem",
+              fontSize: "1.5rem",
               textDecoration: "none",
-              backgroundColor: "#25D366", // WhatsApp color
+              backgroundColor: "#25D366",
               color: "white",
-              padding: "3px 6px",
-              borderRadius: "5px",
-              display: "inline-block", // Ensure the element respects padding and borderRadius
+              padding: "15px 30px",
+              borderRadius: "8px",
+              display: "inline-block",
             }}
           >
             Let's Chat
